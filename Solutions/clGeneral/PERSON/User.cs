337ea159e998a7d11900574b4fbac4fd48bcc76d
Liam <liamc4113@hotmail.co.uk>
2017-10-15 12:43:46 +0100

    First Commit

---
+++ Solutions/clGeneral/PERSON/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace clGeneral.PERSON
+{
+    class User
+    {
+        private void GetUser()
+        {
+
+        }
+    }
+}
